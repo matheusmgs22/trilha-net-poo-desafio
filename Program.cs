f6ed54c1edcfaba87ed4afbcb0ee6617fd418c88
Matheus Garcia <matheusmgs17@gmail.com>
2023-12-28 13:52:53 -0300

    Projeto Concluido!!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,30 @@
-﻿using DesafioPOO.Models;
+﻿using System;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+using DesafioPOO.Models;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Criando um objeto Nokia
+            Console.WriteLine("Smartphone Nokia: ");
+            Smartphone Nokia = new Nokia(numero: "123456789", modelo: "ModeloNokia", imei: "IMEINokia", memoria: 64);
+            Nokia.Ligar();
+            Nokia.ReceberLigacao();
+            Nokia.InstalarAplicativo("Facebook");
+
+            Console.WriteLine("\n");
+
+            // Criando um objeto Iphone
+
+            Console.WriteLine("Smartphone Iphone: ");
+            Smartphone Iphone = new Iphone(numero: "987654321", modelo: "ModeloIphone", imei: "IMEIIphone", memoria: 128);
+            Iphone.Ligar();
+            Iphone.ReceberLigacao();
+            Iphone.InstalarAplicativo("Instagram");
+
+
+        }
+}
+
